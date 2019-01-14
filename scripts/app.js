633c72ad1f2d895320ae71521810f37d39ba8710
Kenneth Choi <kennethichoi@gmail.com>
2019-01-13 21:50:55 -0800

    Project init & add controller, scene, timelines

--- scripts/app.js
+++ scripts/app.js
@@ -1,142 +1,71 @@
-// MAIN 01
-// Wrap every letter in a span
-$(document).ready(
-  $(".main__header").each(function() {
-    $(this).html(
-      $(this)
-        .text()
-        .replace(
-          /[-A-Za-z0-9!$#%^&*@()_+|~=`{}\[\]:";'<>?,.\/]/g,
-          "<span class='letter'>$&</span>"
-        )
-    );
-  })
-);
-// Add anime.js timeline
-let animation1 = () => {
-  anime.timeline({ loop: false }).add({
-    targets: ".main__header .letter",
-    scale: [4, 1],
-    opacity: [0, 1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 500,
-    delay: function(el, i) {
-      return 1000 + 40 * i;
-    }
-  });
-};
+// Fade in on DOM load
+// SCENE 1
+window.addEventListener("DOMContentLoaded", anim1);
 
-// MAIN 02
-// Wrap every letter in a span
-$(document).ready(
-  $(".main2__header").each(function() {
-    $(this).html(
-      $(this)
-        .text()
-        .replace(
-          /[-A-Za-z0-9!$#%^&*@()_+|~=`{}\[\]:";'<>?,.\/]/g,
-          "<span class='letter'>$&</span>"
-        )
-    );
-  })
-);
-// Add anime.js timeline
-let animation2 = () => {
-  anime.timeline({ loop: false }).add(
-    {
-      targets: ".main2__header .letter",
-      scale: [0, 1],
-      opacity: [0, 1],
-      duration: 2000,
-      elasticity: 600,
-      delay: function(el, i) {
-        return 500 + 45 * i;
-      }
-    },
-    0
-  );
-};
-
-// MAIN 03
-// Wrap every letter in a span
-$(document).ready(
-  $(".main3__header").each(function() {
-    $(this).html(
-      $(this)
-        .text()
-        .replace(
-          /[-A-Za-z0-9!$#%^&*@()_+|~=`{}\[\]:";'<>?,.\/]/g,
-          "<span class='letter'>$&</span>"
-        )
-    );
-  })
-);
-// Add anime.js timeline
-let animation3 = () => {
-  anime.timeline({ loop: false }).add({
-    targets: ".main3__header .letter",
-    rotateY: [-90, 0],
-    opacity: [0, 1],
-    duration: 1300,
-    delay: function(el, i) {
-      return 1000 + 45 * i;
-    }
-  });
-};
-
-// MAIN 04
-// Wrap every letter in a span
-$(document).ready(
-  $(".main4__header").each(function() {
-    $(this).html(
-      $(this)
-        .text()
-        .replace(
-          /[-A-Za-z0-9!$#%^&*@()_+|~=`{}\[\]:";'<>?,.\/]/g,
-          "<span class='letter'>$&</span>"
-        )
-    );
-  })
-);
-// Add anime.js timeline
-let animation4 = () => {
-  anime.timeline({ loop: false }).add({
-    targets: ".main4__header .letter",
-    translateX: [40, 0],
-    translateZ: 0,
-    opacity: [0, 1],
-    easing: "easeOutExpo",
-    duration: 1200,
-    delay: function(el, i) {
-      return 1000 + 30 * i;
-    }
-  });
-};
-
-// Intersection Observer API
-let options = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 0
-};
-
-window.addEventListener("DOMContentLoaded", intersectionObserver);
-
-function intersectionObserver() {
-  let observer1 = new IntersectionObserver(animation1, options);
-  let target1 = document.querySelector(".main");
-  observer1.observe(target1);
-
-  let observer2 = new IntersectionObserver(animation2, options);
-  let target2 = document.querySelector(".main2__header");
-  observer2.observe(target2);
-
-  let observer3 = new IntersectionObserver(animation3, options);
-  let target3 = document.querySelector(".main3__header");
-  observer3.observe(target3);
-
-  let observer4 = new IntersectionObserver(animation4, options);
-  let target4 = document.querySelector(".main4__header");
-  observer4.observe(target4);
+function anim1() {
+  let t1 = new TimelineMax();
+  t1.from(".main__header", 1, { opacity: 0, delay: 0.5 });
 }
+
+let t2 = new TimelineMax();
+t2.from(".main2__header", 2.5, {
+  opacity: 0,
+  // rotation: 360,
+  // scale: 0,
+  ease: Power4.easeOut,
+  delay: 0.5
+});
+
+// SCENE 2
+let controller = new ScrollMagic.Controller();
+
+let scene1 = new ScrollMagic.Scene({
+  triggerElement: ".main2",
+  // duration: "70%",
+  offset: 100,
+  reverse: true
+})
+  .addIndicators()
+  .addTo(controller)
+  .setTween(t2);
+
+// SCENE 3
+// let controller = ScrollMagic.Controller();
+let t3 = new TimelineMax();
+t3.from(".main3__header", 2, {
+  scale: 1.2,
+  opacity: 0,
+  ease: Bounce.easeOut,
+  delay: 0.5
+});
+
+let controller2 = new ScrollMagic.Controller();
+
+let scene2 = new ScrollMagic.Scene({
+  triggerElement: ".main3",
+  offset: 100,
+  reverse: true
+})
+  .addIndicators()
+  .addTo(controller2)
+  .setTween(t3);
+
+// SCENE 4
+
+let t4 = new TimelineMax();
+t4.from(".main4", 1, { opacity: 0, ease: Power4.easeOut, delay: 0.5 }).from(
+  ".main4__header",
+  1.5,
+  { opacity: 0, scale: 0.5, ease: Power4.easeOut },
+  "-=1"
+);
+
+let c4 = new ScrollMagic.Controller();
+let scene3 = new ScrollMagic.Scene({
+  triggerElement: ".main4",
+  reverse: true,
+  offset: -100
+})
+  .addIndicators()
+  .setTween(t4)
+  .addTo(c4);
